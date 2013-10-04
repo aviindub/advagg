3c9a8ce47734e27d5ada1d80fabc8d12c7dd7877
mikeytown2 <mikeytown2@282446.no-reply.drupal.org>
2013-10-03 21:06:38 -0400

    Issue #2081977 by mikeytown2: The validator can use JSHint for JS files.

---
+++ advagg_validator/advagg_validator.js
@@ -0,0 +1,37 @@
+
+(function ($) {
+  Drupal.behaviors.advagg_validator = {
+    attach: function (context, settings) {
+      $('.advagg_validator_js', context).click(function (context) {
+        // Get Results Div.
+        var results = $(this).siblings('.results');
+        // Loop over each filename.
+        $.each($(this).siblings('.filenames'), function() {
+          var filename = $(this).val();
+          try {
+            var x = jQuery.ajax({
+              url: settings.basePath + filename,
+              dataType: 'text',
+              async: false
+            });
+            if (JSHINT(x.responseText, Drupal.settings.jshint, Drupal.settings.jshint.predef)) {
+              $(results).append('<h4>' + filename + ' Passed!</h4>');
+            } else {
+              $(results).append('<p><h4>' + filename + ' Failed!</h4>');
+              $(results).append('<ul>');
+              for (i = 0; i < JSHINT.errors.length; i++) {
+                $(results).append('<li><b>' + JSHINT.errors[i].line + ':</b> ' + JSHINT.errors[i].reason + '</li>');
+              }
+              $(results).append('</ul></p>');
+            }
+          }
+          catch (err) {
+            $(results).append(err);
+          }
+        });
+
+        return false;
+      });
+    }
+  };
+}(jQuery));
